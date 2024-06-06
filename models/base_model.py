394f609e1708a6709b037f37ebdbd1f1b519e706
Emeraldolaitan1220 <smallangel2002@gmail.com>
2024-06-06 14:32:26 +0000

    web deploy

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,8 @@ class BaseModel:
     for other classes
     """
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    created_at = Column(DateTime, nullable=False, default=(datetime.now()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.now()))
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
